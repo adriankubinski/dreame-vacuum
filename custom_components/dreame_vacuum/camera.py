3e0ec165f067700915353ddf4c60bc083cbb6911
Adrian Kubiński <32160838+adriankubinski@users.noreply.github.com>
2025-01-30 23:15:43 +0100

    Fix _webrtc_provider error

--- custom_components/dreame_vacuum/camera.py
+++ custom_components/dreame_vacuum/camera.py
@@ -460,7 +460,10 @@ def async_remove_map_cameras(
 
 class DreameVacuumCameraEntity(DreameVacuumEntity, Camera):
     """Defines a Dreame Vacuum Camera entity."""
-
+    _webrtc_provider = None
+    _legacy_webrtc_provider = None
+    _supports_native_sync_webrtc = False
+    _supports_native_async_webrtc = False
     _unrecorded_attributes = frozenset(CAMERA_UNRECORDED_ATTRIBUTES)
 
     def __init__(
